521014d9c9ff6eaad7a1beac14ec5e9b01ffb8a8
iozberk <iozberks@gmail.com>
2022-08-26 15:52:02 +0300

    Producer send message to queue every 3 seconds

--- producer.py
+++ producer.py
@@ -1,11 +1,18 @@
-import pika
+import pika, time
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+
+
+# Producer send message to queue every 3 seconds
+while True:
+    channel.queue_declare(queue='hello')
+
+    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
+    time.sleep(3)
+
 connection.close()
