cd7addd013c5d9a68ab7a7f1feca04f91653684d
iozberk <iozberks@gmail.com>
2022-08-28 14:22:24 +0300

    RabbitMQ producer closed

--- producer1.py
+++ producer1.py
@@ -1,10 +1,14 @@
+from email import message
 import pika 
 
 connection_parameters = pika.ConnectionParameters(host='localhost')
 connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
-
-
+channel.queue_declare(queue='letterbox')
+message = "First message"
+channel.basic_publish(exchange='', routing_key='letterbox', body=message)
+print(f"sent message {message}")
+connection.close()
 
 
 
