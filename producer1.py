f697e93fa6d6da6d0dd96cebc4f00fa911b31094
iozberk <iozberks@gmail.com>
2022-08-28 14:29:26 +0300

    First message to consumer1.py

--- producer1.py
+++ producer1.py
@@ -4,7 +4,7 @@ connection_parameters = pika.ConnectionParameters(host='localhost')
 connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
 channel.queue_declare(queue='letterbox')
-message = "First message"
+message = "First message to consumer1.py"
 channel.basic_publish(exchange='', routing_key='letterbox', body=message)
 print(f"sent message {message}")
 connection.close()
